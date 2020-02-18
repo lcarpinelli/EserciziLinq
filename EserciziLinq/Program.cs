030d0d0ca73941e60a1739acdc915131f1fccc67
l.carpinelli <l.carpinelli@VETRYA-PC100>
2020-02-18 18:02:26 +0100

    Fix

--- EserciziLinq/Program.cs
+++ EserciziLinq/Program.cs
@@ -19,13 +19,13 @@ namespace EserciziLinq
                 new Apple { Color = "Red", Weight = 168, Origin ="Usa"},
                 new Apple { Color = "Green", Weight = 193, Origin ="Italy"},
                 new Apple { Color = "Red", Weight = 176, Origin ="France"},
-                new Apple { Color = "Green", Weight = 210, Origin ="Irland"},
-                new Apple { Color = "Red", Weight = 169, Origin ="Irland"},
+                new Apple { Color = "Green", Weight = 210, Origin ="Ireland"},
+                new Apple { Color = "Red", Weight = 169, Origin ="Ireland"},
                 new Apple { Color = "Green", Weight = 180, Origin ="Germany"},
                 new Apple { Color = "Red", Weight = 185, Origin ="Uk"},
                 new Apple { Color = "Green", Weight = 183, Origin ="France"},
                 new Apple { Color = "Red", Weight = 195, Origin ="Italy"},
-                new Apple { Color = "Green", Weight = 213, Origin ="Irland"},
+                new Apple { Color = "Green", Weight = 213, Origin ="Ireland"},
                 };
 
             //Esercizio 1 Qual è il peso minimo delle mele?
@@ -55,20 +55,6 @@ namespace EserciziLinq
             string originMaxWeight = apples.Where(apple => apple.Weight == maximumWeight).Select(apple => apple.Origin).First();
             Console.WriteLine($"La mela che pesa di più pesa {maximumWeight} e proviene da {originMaxWeight}");
 
-            //Esercizio 7 Qual'è la media del peso di mele? 
-            double averageWeightItaly = apples.Where(apple => apple.Origin == "Italy").Select(apple => apple.Weight).Average();
-            double averageWeightGermany = apples.Where(apple => apple.Origin == "Germany").Select(apple => apple.Weight).Average();
-            double averageWeightUk = apples.Where(apple => apple.Origin == "Uk").Select(apple => apple.Weight).Average();
-            double averageWeightUsa = apples.Where(apple => apple.Origin == "Usa").Select(apple => apple.Weight).Average();
-            double averageWeightFrance = apples.Where(apple => apple.Origin == "France").Select(apple => apple.Weight).Average();
-            double averageWeightIrland = apples.Where(apple => apple.Origin == "Irland").Select(apple => apple.Weight).Average();
-            
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightItaly)} Gr per Italy");
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightFrance)} Gr per France");
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightGermany)} Gr per Germany");
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightIrland)} Gr per Irland");
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightUk)} Gr per Uk");
-            Console.WriteLine($"La media del peso delle mele è {String.Format("{0:0.00}", averageWeightUsa)} Gr per Usa");
         }
     }
 }
